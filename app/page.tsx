76a2358aef321b18864bdb32fefaebf46f7b7478
sae-github <miyasae1126@gmail.com>
2023-08-22 23:01:29 +0900

    ✅: metadataの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next'
 import { HomeContent } from '@/src/components/app/Home'
+
+export const metadata: Metadata = {
+  title: 'Sae/Blog',
+  description: '駆け出しフロントエンドエンジニアの積み上げブログです',
+}
+
 const Home = () => {
   return <HomeContent />
 }
