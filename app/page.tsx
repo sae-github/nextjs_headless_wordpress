a3913c17b6f815c91aab60f5f48adb456c56721c
sae-github <miyasae1126@gmail.com>
2023-09-06 23:36:28 +0900

    ♻️: Metadateをlayoutへ移動

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import { Metadata } from 'next'
 import { HomeContent } from '@/app/components/app/Home'
-
-export const metadata: Metadata = {
-  title: 'Sae/Blog',
-  description: '駆け出しフロントエンドエンジニアの積み上げブログです',
-}
 
 const Home = () => {
   return <HomeContent />
