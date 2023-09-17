08ad8c3f9b0ebd1f538eb041e4100ace8ead16ae
sae-github <miyasae1126@gmail.com>
2023-09-17 15:33:06 +0900

    ✨: 詳細ページに動的にカテゴリーを表示する

--- app/components/app/posts/[id]/index.tsx
+++ app/components/app/posts/[id]/index.tsx
@@ -1,11 +1,12 @@
-import { Box } from '@kuma-ui/core'
+import { Box, HStack } from '@kuma-ui/core'
 import { format } from 'date-fns'
 import React, { FC } from 'react'
 import './style.css'
+import { Tags } from '@/app/components/common/Tags'
 
 const getPost = async (id: string) => {
   const res = await fetch(
-    `https://itosae.com/wp-json/wp/v2/posts/${id}?_fields=id,date,title,content`,
+    `https://itosae.com/wp-json/wp/v2/posts/${id}?_fields=id,date,title,content,categories`,
   )
   if (!res.ok) throw new Error('Failed to fetch data')
   return res.json()
@@ -13,12 +14,16 @@ const getPost = async (id: string) => {
 
 export const PostContent: FC<{ params: { id: string } }> = async ({ params }) => {
   const post = await getPost(params.id)
+
   return (
     <article>
-      <Box>
-        <h1>{post.title.rendered}</h1>
-        <time dateTime={post.date}>{format(new Date(post.date), 'yyyy/MM/dd')}</time>
-      </Box>
+      <h1>{post.title.rendered}</h1>
+      <HStack alignItems='center' gap='8px'>
+        <time dateTime={post.date} style={{ marginTop: '0px' }}>
+          {format(new Date(post.date), 'yyyy/MM/dd')}
+        </time>
+        <Tags categoryIds={post.categories} />
+      </HStack>
       <Box
         marginTop='2rem'
         padding='0 1rem'
