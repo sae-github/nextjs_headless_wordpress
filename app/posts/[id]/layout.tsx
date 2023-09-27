99f7ba93ba4bf160112f7d5c8bb3f528dceeb2ca
sae-github <miyasae1126@gmail.com>
2023-09-28 00:02:42 +0900

    ✅: 詳細ページではOGPを動的にする

--- app/posts/[id]/layout.tsx
+++ app/posts/[id]/layout.tsx
@@ -6,8 +6,22 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   )
   if (!res.ok) throw new Error('Failed to fetch data')
   const post = await res.json()
-
-  return { title: `${post.title.rendered} | Sae/note` }
+  return {
+    title: `${post.title.rendered} | Sae/note`,
+    openGraph: {
+      images: [
+        {
+          url: '/ogp-image.png',
+          width: 80,
+          height: 80,
+        },
+      ],
+      title: post.title.rendered,
+    },
+    twitter: {
+      title: post.title.rendered,
+    },
+  }
 }
 
 export default function PostContentLayout({ children }: { children: React.ReactNode }) {
