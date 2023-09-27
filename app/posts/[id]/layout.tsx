05197c266bbc2a73ce8418ec52991080830a9b41
sae-github <miyasae1126@gmail.com>
2023-09-28 00:06:09 +0900

    ♻️: 1つ上のlayout.tsxと重複している箇所削除

--- app/posts/[id]/layout.tsx
+++ app/posts/[id]/layout.tsx
@@ -9,13 +9,6 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   return {
     title: `${post.title.rendered} | Sae/note`,
     openGraph: {
-      images: [
-        {
-          url: '/ogp-image.png',
-          width: 80,
-          height: 80,
-        },
-      ],
       title: post.title.rendered,
     },
     twitter: {
