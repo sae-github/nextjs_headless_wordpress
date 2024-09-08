8fc0e2928a586e1b61f924c24cbfb3ec35e070c9
sae-github <miyasae1126@gmail.com>
2024-09-08 16:29:00 +0900

    ✅: 投稿の取得ロジックのリファクタリング

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,27 +2,23 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = path.join(process.cwd(), 'output')
-
-export type PostData = {
-  id: string
-  title: string
-  date: string
-  content: string
-}
+const markdownOutPutDirectoryPath = path.join(process.cwd(), 'output')
 
 export function getPosts(): PostData[] {
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostData = fileNames.flatMap((fileName) => {
-    const posts = path.join(process.cwd(), `output/${fileName}`)
-    const readPosts = fs.readdirSync(posts)
-    return readPosts.flatMap((post) => {
-      const a = path.join(process.cwd(), `output/${fileName}/${post}`)
-      const b = fs.readdirSync(a)
-      return b.map((c, index) => {
-        const id = `${fileName}${post}${index + 1}`
-        const fullPath = path.join(process.cwd(), `output/${fileName}/${post}/${c}/index.md`)
-        const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const yearsPaths = fs.readdirSync(markdownOutPutDirectoryPath)
+  const allPostData = yearsPaths.flatMap((year) => {
+    const yearDirPath = path.join(process.cwd(), `output/${year}`)
+    const monthPaths = fs.readdirSync(yearDirPath)
+    return monthPaths.flatMap((month) => {
+      const monthDirPath = path.join(process.cwd(), `output/${year}/${month}`)
+      const pageTitlePaths = fs.readdirSync(monthDirPath)
+      return pageTitlePaths.map((title, index) => {
+        const id = `${year}${month}${index + 1}`
+        const markdownContentPath = path.join(
+          process.cwd(),
+          `output/${year}/${month}/${title}/index.md`,
+        )
+        const fileContents = fs.readFileSync(markdownContentPath, 'utf8')
         const { data, content } = matter(fileContents)
         const postData: PostData = {
           id,
@@ -35,4 +31,10 @@ export function getPosts(): PostData[] {
     })
   })
   return allPostData.sort((a, b) => (a.date < b.date ? 1 : -1))
+}
+export type PostData = {
+  id: string
+  title: string
+  date: string
+  content: string
 }
